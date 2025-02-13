7bc2e86e1193c734e9fa2cf58b09da1fb5ceaab6
Tyler Liu <tyler4long@gmail.com>
2025-02-12 16:00:53 -0800

    Init web phone

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import { createRoot } from "react-dom/client";
+import WebPhone from "ringcentral-web-phone";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<h1>Hello world!</h1>);
+
+const webPhone = new WebPhone({
+  sipInfo: JSON.parse(import.meta.env.VITE_SIP_INFO),
+});
+console.log(webPhone);
