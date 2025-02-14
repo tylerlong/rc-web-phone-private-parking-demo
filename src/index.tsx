115471d86eba1ffe18256f01d4871c3d60b5d4ef
Tyler Liu <tyler4long@gmail.com>
2025-02-14 10:12:23 -0800

    A version works for both deno and vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
-import WebPhone from "ringcentral-web-phone";
+import WebPhone from "https://esm.sh/ringcentral-web-phone@2.0.9";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<h1>Hello world!</h1>);
 
 const webPhone = new WebPhone({
   sipInfo: JSON.parse(import.meta.env.VITE_SIP_INFO),
+  debug: true,
 });
 console.log(webPhone);
+// webPhone.start();
